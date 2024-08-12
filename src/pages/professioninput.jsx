e921d0439ab23b2f88438c3d8f5e211caa5ac561
Suresh-sade <msgmanagementservice@gmail.com>
2024-08-12 19:04:38 +0530

    Delete src/pages directory

--- src/pages/professioninput.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-const Proffessioninput = () => {
-  return (
-    <div className='flex items-center justify-center'>
-    
-    </div>
-  )
-}
-export default Proffessioninput
